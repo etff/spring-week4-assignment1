f3303320df2da0e5978a920f82f7efaad9f4fb4b
etff <wish2rich@gmail.com>
2021-02-26 16:54:53 +0900

    Revert "[Week4] 고양이 장난감가게 만들기"

--- app/src/main/java/com/codesoom/assignment/product/infra/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codesoom.assignment.product.infra;
-
-import com.codesoom.assignment.product.domain.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
